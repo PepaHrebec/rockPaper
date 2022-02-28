1e91dc3db69448be9ae15eed19d25db8808c852f
Josef Hrebec <hrebejos@fit.cvut.cz>
2022-02-28 16:46:54 +0100

    Add buttons and a text response

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const answer = document.querySelector('#answerDiv');
+
+rock.addEventListener("click", function() {playRound(0)});
+paper.addEventListener("click", function() {playRound(1)});
+scissors.addEventListener("click", function() {playRound(2)});
+
 function computerPlay() {
     let val = ((Math.floor(Math.random()*10)) % 3);
     return val;
 };
-function assign(val) {
-    switch(val) {
-        case "rock":
-            return 0;
-        break;
-        case "paper":
-            return 1;
-        break;
-        case "scissors":
-            return 2;
-        break; 
-        default:
-            console.log("Yeah, you've made an error.");
-        break;   
-    }
-}
 
 function decide(humanVal,robotVar){
     if(humanVal===robotVar){
+        answer.textContent="Tie!";
         console.log("Tie!");
     } else {
         switch(humanVal) {
-            case 0:
+            case 0: //rock
                 if (robotVar===1){
+                    answer.textContent="Paper beats rock. You lose!";
                     console.log("Paper beats rock. You lose!");
                 } else {
+                    answer.textContent="Rock beats scissors. You win!";
                     console.log("Rock beats scissors. You win!");
                 }
             break;
             case 1: //paper
                 if (robotVar===2){
+                    answer.textContent="cissors beat paper. You lose!";
                     console.log("Scissors beat paper. You lose!");
                 } else {
+                    answer.textContent="Paper beats rock. You win!";
                     console.log("Paper beats rock. You win!");
                 }
             break;
             case 2: //scissors
                 if (robotVar===0){
+                    answer.textContent="Rock beats scissors. You lose!";
                     console.log("Rock beats scissors. You lose!");
                 } else {
+                    answer.textContent="Scissors beats paper. You win!";
                     console.log("Scissors beats paper. You win!");
                 }
             break;
@@ -49,13 +49,6 @@ function decide(humanVal,robotVar){
     }
 }
 
-function playRound(humanSel) {
-    humanSel=humanSel.toLowerCase();
-    decide(assign(humanSel),computerPlay());
-}
-
-function game() {
-    for(let i=1; i<=5; i++) {
-        playRound(window.prompt("Pick a rock, paper or scissors."));
-    }
+function playRound(humanSel) {   
+    decide(humanSel,computerPlay());
 }
